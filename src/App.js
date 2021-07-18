4f94572330741809825cba1a3ad765f3047b2aaa
caramelmelmel <melody_leong@mymail.sutd.edu.sg>
2021-07-18 16:29:59 +0800

    register scaffold

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import { BrowserRouter as Router, Route} from "react-router-dom";
 
 import './App.css';
 import Login from './components/Login'
+import Register from "./components/Register";
 
 function App() {
   return (
     <Router>
-    <Route exact path='/login' component={Login}/>  
+    <Route exact path='/login' component={Login}/>
+    <Route exact path='/register' component={Register}/>  
     </Router>
   );
 }
