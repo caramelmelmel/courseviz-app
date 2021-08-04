294476c7bab3162360b6591b47d23b8c779c24ea
caramelmelmel <melody_leong@mymail.sutd.edu.sg>
2021-08-04 15:16:05 +0800

    refactored components

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route} from "react-router-dom";
 
 
 import './App.css';
-import SignInSide from './components/Login';
+import SignInSide from './pages/Login';
 import Register from "./components/Register";
 
 function App() {
